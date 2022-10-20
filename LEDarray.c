cddcaf144ff0771ce273c87c8f78b03d04d1d363
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-20 11:48:26 +0100

    Exercise 1.1
    - Set TRIS values to output for the pins connected to the LED array
    - Set ANSEL to 0 (digital) for the respective pins
    - Initialise LAT values 
    - Number the LED -> map the output of the pins to numbers for counting

--- LEDarray.c
+++ LEDarray.c
@@ -7,8 +7,37 @@
 ************************************/
 void LEDarray_init(void)
 {
-    	//set up TRIS registers for pins connected to LED array
+	//set up TRIS registers for pins connected to LED array
+    TRISGbits.TRISG0=0; //set TRIS value for pin (output)
+    TRISGbits.TRISG1=0; //set TRIS value for pin (output)
+    TRISAbits.TRISA2=0; //set TRIS value for pin (output)
+    TRISFbits.TRISF6=0; //set TRIS value for pin (output)
+    TRISAbits.TRISA4=0; //set TRIS value for pin (output)
+    TRISAbits.TRISA5=0; //set TRIS value for pin (output)
+    TRISFbits.TRISF0=0; //set TRIS value for pin (output)
+    TRISBbits.TRISB0=0; //set TRIS value for pin (output)
+    TRISBbits.TRISB1=0; //set TRIS value for pin (output)
+    
+    ANSELGbits.ANSELG0=0; //turn off analogue input on pin 
+    ANSELGbits.ANSELG1=0; //turn off analogue input on pin 
+    ANSELAbits.ANSELA2=0; //turn off analogue input on pin 
+    ANSELFbits.ANSELF6=0; //turn off analogue input on pin 
+    ANSELAbits.ANSELA4=0; //turn off analogue input on pin 
+    ANSELAbits.ANSELA5=0; //turn off analogue input on pin 
+    ANSELFbits.ANSELF0=0; //turn off analogue input on pin 
+    ANSELBbits.ANSELB0=0; //turn off analogue input on pin
+    ANSELBbits.ANSELB1=0; //turn off analogue input on pin
+    
 	//set initial output LAT values (they may have random values when powered on)
+    LATGbits.LATG0=1; //set TRIS value for pin (output)
+    LATGbits.LATG1=1; //set TRIS value for pin (output)
+    LATAbits.LATA2=1; //set TRIS value for pin (output)
+    LATFbits.LATF6=1; //set TRIS value for pin (output)
+    LATAbits.LATA4=1; //set TRIS value for pin (output)
+    LATAbits.LATA5=1; //set TRIS value for pin (output)
+    LATFbits.LATF0=1; //set TRIS value for pin (output)
+    LATBbits.LATB0=1; //set TRIS value for pin (output)
+    LATBbits.LATB1=1; //set TRIS value for pin (output)
 }
 
 /************************************
@@ -19,7 +48,15 @@ void LEDarray_disp_bin(unsigned int number)
 {
 	//some code to turn on/off the pins connected to the LED array
 	//if statements and bit masks can be used to determine if a particular pin should be on/off
-	//see Readme.md for examples
+    if (number & 0b000000001) {LATGbits.LATG0=1;} else {LATGbits.LATG0=0;}
+    if (number & 0b000000010) {LATGbits.LATG1=1;} else {LATGbits.LATG1=0;}
+    if (number & 0b000000100) {LATAbits.LATA2=1;} else {LATAbits.LATA2=0;}
+    if (number & 0b000001000) {LATFbits.LATF6=1;} else {LATFbits.LATF6=0;}
+    if (number & 0b000010000) {LATAbits.LATA4=1;} else {LATAbits.LATA4=0;}
+    if (number & 0b000100000) {LATAbits.LATA5=1;} else {LATAbits.LATA5=0;}
+    if (number & 0b001000000) {LATFbits.LATF0=1;} else {LATFbits.LATF0=0;}
+    if (number & 0b010000000) {LATBbits.LATB0=1;} else {LATBbits.LATB0=0;}
+    if (number & 0b100000000) {LATBbits.LATB1=1;} else {LATBbits.LATB1=0;}
 }
 
 /************************************
@@ -53,4 +90,3 @@ void LEDarray_disp_PPM(unsigned int cur_val, unsigned int max)
 
 	LEDarray_disp_bin(disp_val);	//display value on LED array
 }
-
