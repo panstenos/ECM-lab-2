a046b949722ffff08db0e2cc924436a77e4e16ea
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-20 11:54:37 +0100

    Exercise 1.3
    - Set TRIS and ANSEL for button RF2 to input and analogue respectively
    - Change the function back to counting (when the button is pressed)

--- main.c
+++ main.c
@@ -1,32 +1,30 @@
 #pragma config FEXTOSC = HS     // External Oscillator mode Selection bits (HS (crystal oscillator) above 8 MHz; PFM set to high power)
 #pragma config RSTOSC = EXTOSC_4PLL// Power-up default value for COSC bits (EXTOSC with 4x PLL, with EXTOSC operating per FEXTOSC bits)
-#pragma config WDTE = OFF       // WDT operating mode (WDT enabled regardless of sleep)
+#pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep)
 
 #include <xc.h>
 #include "LEDarray.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
-void main(void) {
+
+void main(void) 
+{
+    // setup pin for input (connected to button)
+    TRISFbits.TRISF2=1; //set TRIS value for pin (input)
+    ANSELFbits.ANSELF2=0; //turn off analogue input on pin 
+
+	unsigned int count=0;
     LEDarray_init();
-    int direction = 2; // direction is 1 for decreasing or 2 for increasing
-    unsigned int number = 1; // define initial count
     
     while (1) {
+        while (PORTFbits.RF2); //empty while loop (wait for button press in RF2 sensor)
         
-        if (direction == 1) {  // loop for decreasing
-            LEDarray_disp_bin(number); //output a on the LED array in binary            
-            __delay_ms(50); // Delay function
-            number = number >> 1; // shift number to the right
-            if (number < 2) {direction = 2;} // when number is less than 2; i.e. 1 change direction
-        }
-        else {
-            LEDarray_disp_bin(number); //output a on the LED array in binary
-            __delay_ms(50); // Delay function
-            number = number << 1; // shift number to the left
-            if (number > 255) {direction = 1;} // when number is more than 255; i.e. 256 change direction
-        }
-
-
+            if (!PORTFbits.RF2){ // when button 'RF2' is pressed
+                count++; // increase the count when the button is pressed
+                }
+		if (count>511) {count=0;} //reset a when it gets too big
+		LEDarray_disp_bin(count); //output a on the LED array in binary
+		__delay_ms(200); // Delay so human eye can see change //original delay 50ms
     }
 }
