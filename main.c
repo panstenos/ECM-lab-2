5f39ccace30051bb04afc176ad10d8d9b7b0c8e3
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-25 20:44:02 +0100

    Exercise 2.1

--- main.c
+++ main.c
@@ -6,28 +6,19 @@
 
 #include <xc.h>
 #include "LEDarray.h"
-#include <stdio.h>
+#include "ADC.h"
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 
 void main(void) 
 {
     // setup pin for input (connected to button)
-    TRISFbits.TRISF2=1; //set TRIS value for pin (input)
-    ANSELFbits.ANSELF2=0; //turn off analogue input on pin 
-
-	unsigned int count=0;
     LEDarray_init();
+    ADC_init();
     
     while (1) {
-        while (PORTFbits.RF2); //empty while loop (wait for button press in RF2 sensor)
-        
-            if (!PORTFbits.RF2){ // when button 'RF2' is pressed
-                count++; // increase the count when the button is pressed
-                }
-		if (count>99) {count=0;} //reset a when it gets too big
-		LEDarray_disp_dec(count); //output a on the LED array in binary
-        //(count);
-		__delay_ms(200); // Reset back to 200 to see the change
+        LEDarray_disp_bin(ADC_getval());
+		__delay_ms(100); // Delay so human eye can see change //original delay 50ms
     }
 }
+
