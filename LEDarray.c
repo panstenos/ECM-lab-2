ff1df34126b6e320e08fa2bdc5eff6350be99011
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-26 04:25:03 +0100

    Exercise 3 with comments
    all useless functions removed

--- LEDarray.c
+++ LEDarray.c
@@ -3,11 +3,12 @@
 
 /************************************
 / LEDarray_init
-/ Function used to initialise pins to drivcvxc           e the LEDarray
+/ Function used to initialise pins connected to the LEDarray
 ************************************/
 void LEDarray_init(void)
 {
-	//set up TRIS registers for pins connected to LED array
+	//set up TRIS registers for LEDa array connected pins 
+    //  -> initialise all to 0 (output)
     TRISGbits.TRISG0=0; //set TRIS value for pin (output)
     TRISGbits.TRISG1=0; //set TRIS value for pin (output)
     TRISAbits.TRISA2=0; //set TRIS value for pin (output)
@@ -18,6 +19,8 @@ void LEDarray_init(void)
     TRISBbits.TRISB0=0; //set TRIS value for pin (output)
     TRISBbits.TRISB1=0; //set TRIS value for pin (output)
     
+    //set up the ANSEL registers for the LED array connected pins 
+    //  -> initialise to 0 (digital)
     ANSELGbits.ANSELG0=0; //turn off analogue input on pin 
     ANSELGbits.ANSELG1=0; //turn off analogue input on pin 
     ANSELAbits.ANSELA2=0; //turn off analogue input on pin 
@@ -28,7 +31,7 @@ void LEDarray_init(void)
     ANSELBbits.ANSELB0=0; //turn off analogue input on pin
     ANSELBbits.ANSELB1=0; //turn off analogue input on pin
     
-	//set initial output LAT values (they may have random values when powered on)
+	//set up the initial output of LAT values for the LED array connected pins
     LATGbits.LATG0=0; //set TRIS value for pin (output)
     LATGbits.LATG1=0; //set TRIS value for pin (output)
     LATAbits.LATA2=0; //set TRIS value for pin (output)
@@ -47,7 +50,8 @@ void LEDarray_init(void)
 void LEDarray_disp_bin(unsigned int number)
 {
 	//some code to turn on/off the pins connected to the LED array
-	//if statements and bit masks can be used to determine if a particular pin should be on/off
+	//if statements and bit masks can be used to determine 
+    //  if a particular pin should be on/off
     if (number & 0b000000001) {LATGbits.LATG0=1;} else {LATGbits.LATG0=0;}
     if (number & 0b000000010) {LATGbits.LATG1=1;} else {LATGbits.LATG1=0;}
     if (number & 0b000000100) {LATAbits.LATA2=1;} else {LATAbits.LATA2=0;}
@@ -60,42 +64,20 @@ void LEDarray_disp_bin(unsigned int number)
 }
 
 /************************************
-/ Function LEDarray_disp_dec
-/ Used to display a number on the LEDs
-/ where each LED is a value of 10
-************************************/
-void LEDarray_disp_dec(unsigned int number)
-{
-	unsigned int disp_val;
-	disp_val = 0;
-    //some code to manipulate the variable number into the correct
-    //format and store in disp_val for display on the LED array  
-    if (number <= 9) {disp_val = 0;}    
-    if (number >  9) {disp_val = 1;}
-    if (number > 19) {disp_val = 3;}
-    if (number > 29) {disp_val = 7;}
-    if (number > 39) {disp_val = 15;}
-    if (number > 49) {disp_val = 31;}
-    if (number > 59) {disp_val = 63;}
-    if (number > 69) {disp_val = 127;}
-    if (number > 79) {disp_val = 255;}
-    if (number > 89) {disp_val = 511;}
-	LEDarray_disp_bin(disp_val); 	//display value on LED array
-}
-
-/************************************
 / LEDarray_disp_PPM
 / Function used to display a level on the LED array with peak hold
-/ cur_val is the current level from the most recent sample, and max is the peak value for the last second
+/ cur_val is the current level from the most recent sample, 
+/   and max is the peak value for the last second
 / these input values need to calculated else where in your code
 ************************************/
 void LEDarray_disp_PPM(unsigned int cur_val, unsigned int max)
 {
         
-    unsigned int con_curr;	
-    con_curr = 0;
-    // convert current value to multiple LED output 
-    // split the resistance input to deciles
+    unsigned int con_curr; //define con_curr value
+    con_curr = 0; //initialise con_curr value
+    
+    // convert current value to multiple LED output as specified in Ex3
+    // split the resistance input to deciles -> 255/10 = 25.5
     if (cur_val <= 25.5) {con_curr = 0;}    
     if (cur_val >  25.5) {con_curr = 1;}
     if (cur_val > 51.0) {con_curr = 3;}
@@ -107,10 +89,11 @@ void LEDarray_disp_PPM(unsigned int cur_val, unsigned int max)
     if (cur_val > 204) {con_curr = 255;}
     if (cur_val > 229.5) {con_curr = 511;}
     
-	unsigned int con_max;
-	con_max = 0;
-    // convert max value to single LED output 
-    // split the resistance input to deciles
+	unsigned int con_max; //define con_max value
+	con_max = 0; //initialise con_max value
+    
+    // convert max value to single LED output as specified in Ex3
+    // split the 'max' value stored to deciles
     if (max <= 25.5) {con_max = 0;}    
     if (max >  25.5) {con_max = 1;}
     if (max > 51.0) {con_max = 2;}
@@ -122,11 +105,13 @@ void LEDarray_disp_PPM(unsigned int cur_val, unsigned int max)
     if (max > 204) {con_max = 128;}
     if (max > 229.5) {con_max = 256;}
     
-    unsigned int disp_val; 
+    unsigned int disp_val; //define disp_val
     
+    //use a bitwise OR operation to add 'con_max' and 'con_curr'
     disp_val = con_max | con_curr;
 	LEDarray_disp_bin(disp_val);	//display value on LED array
 }
+
 
 
 
